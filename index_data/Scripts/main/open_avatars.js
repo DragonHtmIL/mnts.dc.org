2fbbf8f786a39955a2b9dc2d5dc1078ba3acdd49
DragonHtmIL <meirfons1@gmail.com>
2025-12-19 02:31:19 +0200

    Delete index_data directory

--- index_data/Scripts/main/open_avatars.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("btnlocateavatars").onclick = function() {
-  var btnClose = document.getElementById("btnclosewindows");
-  document.getElementById("iAvatars").style.right = "0%";
-  document.getElementById("plustitle").style.opacity = "0";
-  document.getElementById("plustitleAvatars").style.opacity = "1";
-  btnClose.style.marginTop = "0%";
-  btnClose.style.borderBottom = "none";
-  document.body.style.overflowY = "hidden";
-}
--- index_data/Scripts/wallpapers/pagination_tabs.js
+++
@@ -1,9 +0,0 @@
-function openPings(evt, pingName) {
-  var i, paginationcontainer;
-  paginationcontainer = document.getElementsByClassName("paginationcontainer");
-  for (i = 0; i < paginationcontainer.length; i++) {
-    paginationcontainer[i].style.display = "none";
-  }
-  document.getElementById(pingName).style.display = "inline-flex";
-  evt.currentTarget.className += " flexed";
-};
