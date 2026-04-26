f114360c4dd206736657651bf70be01c59fa8493
ArthurVesta <meirfons1@gmail.com>
2026-04-26 14:25:29 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/mnts.dc.org';
 var APP_PREFIX = 'gppwa_';
-var VERSION = "0.1.5-26042026";
+var VERSION = "0.1.6-26042026";
 var URLS = [
   `${GHPATH}/index_data/Scripts/avatars/avatar_ai_loader.js`,
   `${GHPATH}/index_data/Scripts/avatars/avatar_animated_loader.js`,
